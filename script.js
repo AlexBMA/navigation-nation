6a23aa3d3685215ad819b92bdcb799a1dd46d2d6
Alexandru Burghelea <burghelea.alex@gmail.com>
2020-08-26 10:03:38 +0300

    more refactor done

--- script.js
+++ script.js
@@ -34,8 +34,5 @@ function toggleNav() {
 
 //Event Listeners
 menuBars.addEventListener('click', toggleNav);
-nav1.addEventListener('click', toggleNav);
-nav2.addEventListener('click', toggleNav);
-nav3.addEventListener('click', toggleNav);
-nav4.addEventListener('click', toggleNav);
-nav5.addEventListener('click', toggleNav);
+nav.forEach(item=>item.addEventListener('click',toggleNav));
+
